e0236604ca9ece0a68fcba4fbb2906bfb75cf0e0
Nastya Sakun <sakun_nastya@mail.ru>
2018-10-25 21:55:18 +0300

    Добавьте файлы проекта.

---
+++ partial class Skameika.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Work6;
+
+namespace Work6
+{
+    public partial class Skameika
+
+    {
+        public int E = 28;
+
+        public override string ToString()
+        {
+            return ($"\0Информация об объекте:\0{E},\0{E.Equals(6)},\0{E.GetHashCode()},\0{E.GetType()}");
+        }
+    }
+}
